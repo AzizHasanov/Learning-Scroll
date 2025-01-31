b7e7f3815de412c391b32003b9e531f10689d360
aziz <aziz@home-ubuntu1>
2025-02-01 02:27:59 +0900

    initial project upload

---
+++ learning-scroll-server/src/main/java/com/covenant_scholar/learning_scroll_server/LearningScrollServerApplication.java
@@ -0,0 +1,13 @@
+package com.covenant_scholar.learning_scroll_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningScrollServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningScrollServerApplication.class, args);
+	}
+
+}
---
+++ learning-scroll-server/src/test/java/com/covenant_scholar/learning_scroll_server/LearningScrollServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covenant_scholar.learning_scroll_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningScrollServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
